0b5c7fb530f241316d45cb4e63e79ab9039dba23
Kitti_pop <jokkerlolyer20@gmail.com>
2021-03-23 09:55:39 +0700

    parallax demo

---
+++ src/components/animate/AutoRotate.vue
@@ -0,0 +1,44 @@
+<template>
+  <img class="rotate" ref="rotateRef" :src="this.image" />
+</template>
+
+<script>
+export default {
+  name: "AutoRotate",
+  props: ["image", "duration", "reverse"],
+  created() {
+    const animation = this.reverse ?? false ? "reverseRotation " : "rotation ";
+    setTimeout(() => {
+      this.$refs.rotateRef.style.animation =
+        animation + (this.duration ?? 128) + "s infinite linear";
+    }, 200);
+  },
+};
+</script>
+
+<style>
+.rotate {
+  width: 12vw;
+  height: 12vw;
+  object-fit: contain;
+  /* animation: rotation 128s infinite linear; */
+}
+
+@keyframes rotation {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(359deg);
+  }
+}
+
+@keyframes reverseRotation {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(-359deg);
+  }
+}
+</style>
---
+++ src/components/animate/HoverRotate.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hoverImage">
+    <auto-rotate
+      :image="this.image"
+      :duration="this.duration"
+      :reverse="this.reverse"
+    ></auto-rotate>
+  </div>
+</template>
+
+<script>
+import AutoRotate from "./AutoRotate.vue";
+export default {
+  components: { AutoRotate },
+  name: "HoverRotate",
+  props: ["image", "duration", "reverse"],
+};
+</script>
+
+<style>
+.hoverImage {
+  width: 12vw;
+  height: 12vw;
+  object-fit: contain;
+  animation: scaleBack 0.125s 1 linear;
+}
+.hoverImage:hover {
+  animation: scaleForward 0.125s 1 linear;
+  transform: scale(1.1);
+}
+
+@keyframes scaleForward {
+  from {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(1.1);
+  }
+}
+@keyframes scaleBack {
+  from {
+    transform: scale(1.1);
+  }
+  to {
+    transform: scale(1);
+  }
+}
+</style>
