5cd2e20662e639e032895b433f8b841187db334a
Kitti_pop <jokkerlolyer20@gmail.com>
2021-03-23 16:18:43 +0700

    simple physics

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
-  <div></div>
+  <div class="root">
+    <physics-box></physics-box>
+  </div>
 </template>
 
 <script>
+import PhysicsBox from "./components/PhysicsBox.vue";
 export default {
+  components: { PhysicsBox },
   name: "App",
 };
 </script>
 
 <style>
+.root {
+  height: 100vh;
+  width: 100vw;
+  background-color: bisque;
+}
 </style>
